6d0010e941d358adc11e0a993a8840c95402e9c2
ST10091894 <ST10091894@vcconnect.edu.za>
2023-12-15 10:39:51 +0200

    updated design of the create task fragment

--- app/src/main/java/com/thabiso81/taskwiz/view/fragments/CreateTaskFragment.kt
+++ app/src/main/java/com/thabiso81/taskwiz/view/fragments/CreateTaskFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -109,7 +110,7 @@ class CreateTaskFragment : Fragment() {
         }
     }
 
-    fun inputValid(taskName: EditText, taskDueDate: EditText): Boolean{
+    fun inputValid(taskName: EditText, taskDueDate: TextView): Boolean{
         var isValid = true
         if (taskName.text.toString().isEmpty()){
             isValid = false
